62898a36383b1fd1df1de2b3f0b8da440e661b98
Karina-N <67471786+Karina-N@users.noreply.github.com>
2021-07-21 10:28:52 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,27 +1,27 @@
-const baseIngredients = [{ name: "taco shell", img: "/images/taco-shell.png" }];
+const baseIngredients = [{ name: "taco shell", img: "../images/taco-shell.png" }];
 const randomIngredients = [
-  { name: "beef", img: "/images/beef.png" },
-  { name: "beans", img: "/images/beans.png" },
-  { name: "tomatoes", img: "/images/tomatoes.png" },
-  { name: "lettuce", img: "/images/lettuce.png" },
-  { name: "onions", img: "/images/onions.png" },
-  { name: "cheese", img: "/images/cheese.png" },
-  { name: "guacamole", img: "/images/guacamole.png" },
-  { name: "sweetcorn", img: "/images/sweetcorn.png" },
-  { name: "jalapenos", img: "/images/jalapenos.png" },
+  { name: "beef", img: "../images/beef.png" },
+  { name: "beans", img: "../images/beans.png" },
+  { name: "tomatoes", img: "../images/tomatoes.png" },
+  { name: "lettuce", img: "../images/lettuce.png" },
+  { name: "onions", img: "../images/onions.png" },
+  { name: "cheese", img: "../images/cheese.png" },
+  { name: "guacamole", img: "../images/guacamole.png" },
+  { name: "sweetcorn", img: "../images/sweetcorn.png" },
+  { name: "jalapenos", img: "../images/jalapenos.png" },
 ];
 
-// const baseIngredients = [{ name: "bun", img: "/images/bun.png" }];
+// const baseIngredients = [{ name: "bun", img: "../images/bun.png" }];
 // const randomIngredients = [
-//   { name: "burger patty", img: "/images/burger-patty.png" },
-//   { name: "pickles", img: "/images/pickles.png" },
-//   { name: "tomatoes", img: "/images/tomatoes.png" },
-//   { name: "lettuce", img: "/images/lettuce.png" },
-//   { name: "onions", img: "/images/onions.png" },
-//   { name: "cheese slice", img: "/images/cheese-slice.png" },
-//   { name: "onion rings", img: "/images/onion-rings.png" },
-//   { name: "bacon", img: "/images/bacon.png" },
-//   { name: "jalapenos", img: "/images/jalapenos.png" },
+//   { name: "burger patty", img: "../images/burger-patty.png" },
+//   { name: "pickles", img: "../images/pickles.png" },
+//   { name: "tomatoes", img: "../images/tomatoes.png" },
+//   { name: "lettuce", img: "../images/lettuce.png" },
+//   { name: "onions", img: "../images/onions.png" },
+//   { name: "cheese slice", img: "../images/cheese-slice.png" },
+//   { name: "onion rings", img: "../images/onion-rings.png" },
+//   { name: "bacon", img: "../images/bacon.png" },
+//   { name: "jalapenos", img: "../images/jalapenos.png" },
 // ];
 
 export { baseIngredients, randomIngredients };
